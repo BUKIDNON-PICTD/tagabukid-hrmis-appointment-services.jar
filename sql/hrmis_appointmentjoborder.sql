ede95c1cc07aff88457538d49104ff12c2fe72e8
dabidpol <dcabatingan@gmail.com>
2018-05-30 09:14:06 +0800

    changes

--- sql/hrmis_appointmentjoborder.sql
+++ sql/hrmis_appointmentjoborder.sql
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 [getGroupName]
 select appointmentgroupname
 from hrmis_appointmentjoborder
 where appointmentgroupname like $P{searchtext}
 group by appointmentgroupname
-=======
-[getGroupName]
-select appointmentgroupname
-from hrmis_appointmentjoborder
-where appointmentgroupname like $P{searchtext}
-group by appointmentgroupname
-
->>>>>>> ec054f5d6371edf58f11dcb3cb69bc11d5619c2b
