ac59805915503a4b5e235548de3536bb51197311
rufdev <aguilarufino@gmail.com>
2018-06-22 16:46:47 +0800

    integrated plantilla management to casual appointment

--- sql/hrmis_appointmentcasual.sql
+++ sql/hrmis_appointmentcasual.sql
@@ -1,5 +1,13 @@
 [getList]
 SELECT DISTINCT xx.`appointmentgroupname`,xx.objid
 FROM hrmis_appointmentcasual xx
-WHERE NOW() BETWEEN xx.`effectivefrom` AND `xx.`effectiveuntil`
+WHERE NOW() BETWEEN xx.`effectivefrom` AND xx.`effectiveuntil`
 WHERE xx. appointmentgroupname LIKE $P{searchtext}
+
+[findDailyWageByTranch]
+SELECT si.* FROM hrmis_tblemploymentplantilla p
+INNER JOIN references_tbljobposition j ON j.`objid` = p.`jobposition_objid`
+INNER JOIN hrmis_tblpayrollsalaryscheduleitem si ON si.`paygradeandstepincrementid` = j.`paygradeid`
+WHERE si.salarytrancheid = $P{trancheid}
+AND p.objid = $P{plantillaid};
+
