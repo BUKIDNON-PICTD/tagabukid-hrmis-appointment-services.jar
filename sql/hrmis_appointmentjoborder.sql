11dca64c7668038808a09735b7d60ee3fe6e5cae
dabidpol <dcabatingan@gmail.com>
2018-07-06 14:14:21 +0800

    added support service for JO tranche churvs

--- sql/hrmis_appointmentjoborder.sql
+++ sql/hrmis_appointmentjoborder.sql
@@ -3,3 +3,9 @@ select appointmentgroupname
 from hrmis_appointmentjoborder
 where appointmentgroupname like $P{searchtext}
 group by appointmentgroupname
+
+[findDailyWageByTranch]
+SELECT si.* FROM references_tbljobposition j
+INNER JOIN hrmis_tblpayrollsalaryscheduleitem si ON si.`paygradeandstepincrementid` = j.`paygradeid`
+WHERE si.salarytrancheid = $P{trancheid}
+AND j.objid = $P{jobpositionid}
