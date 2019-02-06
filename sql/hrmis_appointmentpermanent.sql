87da0a0b334a5572ef19f143c232502646d38230
rufdev <aguilarufino@gmail.com>
2019-02-06 09:39:35 +0800

    added nosa for new appointment

--- sql/hrmis_appointmentpermanent.sql
+++ sql/hrmis_appointmentpermanent.sql
@@ -16,3 +16,21 @@ SELECT x.* FROM hrmis_appointmentcasualitems xx
 INNER JOIN etracs254_bukidnon.entityindividual i ON i.objid = xx.personnel_objid
 WHERE xx.`parentid` = $P{parentid}
 ORDER BY i.lastname
+
+[getOriginalAppointment]
+SELECT 
+"ISSUED" as status,
+p.`objid` AS appointment_objid,
+t.`objid` AS tranche_objid,
+i.`objid` AS paygradestep_objid,
+p.`effectivefrom`,
+p.`effectivefrom`,
+i.`amount`,
+gs.`grade`,
+gs.`step`
+FROM hrmis_appointmentpermanent p
+INNER JOIN hrmis_tblpayrollsalaryscheduleitem i ON i.`paygradeandstepincrementid` = p.`paygradeandstepincrement_objid`
+INNER JOIN references_tblpaygradeandstepincrement gs ON gs.objid = i.`paygradeandstepincrementid`
+INNER JOIN hrmis_tblpayrollsalarytranche t ON i.`salarytrancheid` = t.objid 
+WHERE (p.`effectivefrom` BETWEEN t.`effectivefromdate` AND t.`effectivetodate`)
+AND gs.`step` = 1) AND p.objid = $P{objid}
