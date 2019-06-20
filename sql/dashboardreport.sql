8f3224128eeab8106dbd2d58817e68f1bea5fa3e
Hoyosjade27 <hoyosjade27@gmail.com>
2019-06-20 12:45:31 +0800

    added EtracsDashReport

---
+++ sql/dashboardreport.sql
@@ -0,0 +1,15 @@
+[getDashrputypecount]
+SELECT
+mu.name, 
+faas.barangay,
+SUM(IF(rpu.rputype = 'land',1,0)) AS LAND,
+SUM(IF(rpu.rputype = 'bldg',1,0)) AS BLDG,
+SUM(IF(rpu.rputype = 'misc',1,0)) AS MISC,
+SUM(IF(rpu.rputype = 'planttree',1,0)) AS PLANTTREE,
+SUM(IF(rpu.rputype = 'mach',1,0)) AS MACH
+FROM faas_list faas
+INNER JOIN municipality mu ON mu.objid = faas.lguid
+INNER JOIN rpu rpu ON rpu.objid = faas.rpuid
+WHERE faas.state = "CURRENT"  
+GROUP BY mu.name, faas.barangay
+ORDER BY mu.name;
--- sql/hrmis_appointmentdash.sql
+++ sql/hrmis_appointmentdash.sql
@@ -1,2 +1,16 @@
 [getTiles]
-SELECT * FROM hrmis_dash_widget WHERE state = 'ACTIVE' order by sortorder;
+SELECT * FROM hrmis_dash_widget WHERE state = 'ACTIVE';
+
+-- [getDashrputypecount]
+-- SELECT mu.name, faas.barangay,
+-- SUM(IF(rpu.rputype = 'land',1,0)) AS LAND,
+-- SUM(IF(rpu.rputype = 'bldg',1,0)) AS BLDG,
+-- SUM(IF(rpu.rputype = 'misc',1,0)) AS MISC,
+-- SUM(IF(rpu.rputype = 'planttree',1,0)) AS PLANTTREE,
+-- SUM(IF(rpu.rputype = 'mach',1,0)) AS MACH
+-- FROM faas_list faas
+-- INNER JOIN municipality mu ON mu.objid = faas.lguid
+-- INNER JOIN rpu rpu ON rpu.objid = faas.rpuid
+-- WHERE faas.state = "CURRENT"  
+-- GROUP BY mu.name, faas.barangay
+-- ORDER BY mu.name;
